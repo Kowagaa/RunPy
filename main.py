be5b5ffdac80418648df0a1022ff5fbd19085e09
Lospros48483 <losamigosdeguille@gmail.com>
2025-03-05 16:11:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import os
+file = open("code.py", "w")
+ask = input("Type ONE python line of code ")
+run = " "
+file.write(ask)
+run = input("Do yo want to run it? (y or n)? ")
+while:
+  while run = "n":
+    print(" ")
+    ask = input("Type ONE python line of code: ")
+    file.write(ask)
+    run = input("Do yo want to run it? (y or n)? ")
+    os.system("clear")
+  while run = "y":
+    os.system("clear")
+    os.system("python code.py")
+    run = input("Do yo want to run it? (y or n)? ")
 
