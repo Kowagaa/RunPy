ca7f59064ee541cec2d8854347e7f9b1b35061de
Lospros48483 <losamigosdeguille@gmail.com>
2025-03-05 16:33:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ run = " "
 file.write(ask)
 run = input("Do yo want to run it? (y or n)? ")
 while True:
-  while run = "n":
+  while run == "n":
     print(" ")
     ask = input("Type ONE python line of code: ")
     file.write(ask)
     run = input("Do yo want to run it? (y or n)? ")
     os.system("clear")
-  while run = "y":
+  while run == "y":
     os.system("clear")
     os.system("python code.py")
     run = input("Do yo want to run it? (y or n)? ")
