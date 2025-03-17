3dd9b18bbd56a9d5e2b94aaed80f12c347142eea
Kowaga <encrypter.dude@gmail.com>
2025-03-17 15:29:07 +0000

    oops forgot to save main.py

--- main.py
+++ main.py
@@ -1,10 +1,2 @@
-import os
-yes_or_no_dict = {"y" : True, "n" : False,}
-def newLine():
-  global yes_or_no_dict
-  if yes_or_no_dict[input("New Line? [y/n] :\n").lower()]:
-    open("code.py", 'w').write(input("Input ONE python line of code: \n"))
-  if yes_or_no_dict[input("Run? [y/n] :\n").lower()]:
-    os.system("python code.py")
-while newLine() != "exit":
-  os.system("clear")
+while eval(input("Please input Line to be executed \n")) != "exit":
+  print("\n")
